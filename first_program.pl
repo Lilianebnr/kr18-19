8bdca8d71a6e76013531ae10040e49c1b82348ae
liliane benamar <liliane.benamar469@alu.uhu.es>
2019-03-13 17:47:54 +0100

    first program

---
+++ first_program.pl
@@ -0,0 +1,16 @@
+
+% man(Person)
+% it is true if Person is a man
+
+man(justus).
+man(lars).
+man(teun).
+man(sebstian).
+
+
+% woman(Person)
+% it is true if Person is a woman
+woman(wendele).
+
+
+
